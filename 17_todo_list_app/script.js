ea966500789e44fa4dc824074a95950a29ef2ee1
kevinWebDev1 <kevinbusiness62@gmail.com>
2025-04-27 21:17:47 +0530

    17_todo_list_app

---
+++ 17_todo_list_app/script.js
@@ -0,0 +1,58 @@
+//✅ 1. Create an Array called todos
+const todos = [
+    {id: 1, task: "Go to gym", completed: true } 
+]
+
+// ✅ 2. Functions to create:
+// addTodo(task) → Add a new todo with completed: false.
+// completeTodo(id) → Mark a todo as completed.
+// deleteTodo(id) → Delete a todo by id.
+// listTodos() → Print all todos showing (✔️ / ❌) based on completed status.
+let idCounter = 2;
+const addTodo = (task) => {
+    todos.push({ id: idCounter++, task: task, completed: false });
+};
+const completeTodo = id => {
+    todos.forEach(item => {
+        if(item.id === id) {
+            item.completed = true;
+        }
+    })
+} 
+
+const deleteTodo = id => {
+    const index = todos.findIndex(item => item.id === id);
+    if(index !== -1) {
+        return todos.splice(index,1);
+    } else {
+        return "Item not found";
+    }
+    
+}
+
+const remainingTodos = () => {
+    let remainingTodos = 0;
+    todos.forEach(todo => {
+        if(todo.completed === false) {
+            remainingTodos++;
+        }
+    });
+    return console.log(`remaining Todos => ${remainingTodos}`);
+}
+
+const listTodos = () => {
+    remainingTodos();
+    todos.forEach(item => {
+        const status = item.completed ? "✔️" : "❌";
+        console.log(`${status} ${item.task}`);
+    });
+};
+ 
+addTodo("Go to Shop");
+addTodo("Read a Book");
+addTodo("Write a Blog Post");
+addTodo("Cook Dinner");
+addTodo("Call Parents");
+completeTodo(6);
+deleteTodo(2);
+listTodos();
